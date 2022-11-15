c5eac0b4908f57b07c30891ba42345ab3efa8153
viscum <silence_fl@yeah.net>
2022-11-15 21:37:38 +0800

    update

--- common/src/main/java/com/viscum/common/enums/CarPlateColorEnum.java
+++ common/src/main/java/com/viscum/common/enums/CarPlateColorEnum.java
@@ -2,11 +2,24 @@ package com.viscum.common.enums;
 
 public enum CarPlateColorEnum {
 
+	/**
+	 * 蓝牌
+	 */
 	BLUE,
+	/**
+	 * 黄牌
+	 */
 	YELLOW,
-
+	/**
+	 * 绿牌
+	 */
 	GREEN,
-
+	/**
+	 * 白牌
+	 */
 	WHITE,
+	/**
+	 * 黑牌
+	 */
 	BLACK;
 }
